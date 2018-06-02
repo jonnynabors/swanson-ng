7a5bd0cf93f53111d11b5db4867aca27aa981faf
Jonny Nabors <jonnynabors@gmail.com>
2018-06-01 20:33:46 -0400

    Add api service

---
+++ src/app/api.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@angular/core';
+import { Http } from '@angular/http';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable()
+export class ApiService {
+  constructor(private http: HttpClient) {}
+
+  public getSingleQuote() {
+    return this.http.get('http://ron-swanson-quotes.herokuapp.com/v2/quotes');
+  }
+}
