3d105b1e2e474c8719aa4b1dc04c47747da3d895
Jonny Nabors <jonnynabors@gmail.com>
2018-06-03 13:01:08 -0400

    Add component test

--- src/app/single-quote/single-quote.component.spec.ts
+++ src/app/single-quote/single-quote.component.spec.ts
@@ -1,9 +1,19 @@
+import { ApiService } from './../api.service';
+import { mock, instance, when } from 'ts-mockito';
 import { SingleQuoteComponent } from './single-quote.component';
+import { Observable } from 'rxjs';
+import 'rxjs/add/Observable/of';
 
 describe('SingleQuoteComponent', () => {
-  beforeEach(() => {});
 
-  it('should create', () => {
-    expect(true).toBeTruthy();
+  it('should set a value for quotes from the api service', () => {
+    const mockApiService: ApiService = mock(ApiService);
+    const expectedApiResult = [
+      'Keep your tears in your eyes where they belong.'
+    ];
+    const singleQuoteComponent: SingleQuoteComponent = new SingleQuoteComponent(instance(mockApiService));
+    when(mockApiService.getSingleQuote()).thenReturn(Observable.of(expectedApiResult));
+    singleQuoteComponent.ngOnInit();
+    expect(singleQuoteComponent.quotes).toEqual(expectedApiResult);
   });
 });
